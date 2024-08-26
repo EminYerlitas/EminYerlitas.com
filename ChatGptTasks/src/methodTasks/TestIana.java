8fd0eae7f842220d78795e48c2aa728339051907
iana27 <ianocika2787@yahoo.com>
2024-08-26 09:53:50 -0400

    Iana's commit

---
+++ ChatGptTasks/src/methodTasks/TestIana.java
@@ -0,0 +1,16 @@
+package methodTasks;
+
+public class TestIana {
+
+	public static void main(String[] args) {
+		boolean lifeIsBeautiful=true;
+		
+		if(lifeIsBeautiful) {
+			String enjoy="Enjoy, make kids, have fun";
+		}else {
+			String noElse="Life is Beautiful.Enjoy, make kids, have fun!Period";
+		}
+
+	}
+
+}
